aaad6fc95204dd0ad11fdf129f174ac30311c9b9
Jorge R <jorgerodriguez434@gmail.com>
2018-08-27 17:27:33 -0500

    change to orange

--- src/components/links.js
+++ src/components/links.js
@@ -3,12 +3,16 @@ import React from "react";
 import { HashLink as Link } from 'react-router-hash-link';
 
 export default class Links extends React.Component {
+
+    gitHub(){
+        window.location.assign("https://github.com/jorgerodriguez434");
+    }
   render() {
     return (
       <div className="link-group">
         <Link className="link" to="/bio#bio">Bio</Link>
         <Link className="link" to="/portfolio#portfolio">Portfolio</Link>
-        <Link className="link" to="/github">Github</Link>
+        <Link className="link" onClick={this.gitHub} to="/github">Github</Link>
       </div>
     );
   }
