929b04b20adbdb11bd6f08d11a7c651857d45e6b
Jorge R <jorgerodriguez434@gmail.com>
2018-10-15 14:42:55 -0500

    change theme

--- src/components/bio.js
+++ src/components/bio.js
@@ -3,29 +3,38 @@ import React from "react";
 export default class Bio extends React.Component {
   render() {
     return (
-      <div id="bio">
-       <h2> About Me</h2>
+      <section id="bio" className="about-me-container">
+   
+      <div className="me"> 
         <img
           src="https://previews.dropbox.com/p/thumb/AANPvCwSVc1RxsVqPIL_RsBO_VKIFMg6LcPOTIt-uTBrKAqJANJhU1Zr4PJCQtWguuo09I71QNgnfNwds1grxWmJc3P6E8gN_nHY5XWZ9viqMu9vfgCfWrMkiGvhWcKXH-H6fAJjbMEKDRWKTFmBw5ydwoaYvsOYQBO3Gaoqy0X4lquzn1GKRe-rJthp5BMHj6b8W5pKL3fIekqe57d4Z-17w8XMK5Z2G3X4sxg4cj5-Ug/p.jpeg?size=2048x1536&size_mode=3"
           alt="skydiving"
+          className="circle"
         />
-        <section className="bio-div">
-          <div className="bio-intro">
-            <p className=""> Here's a little bit about me: </p>
+      </div>
+        <div className="bio-div">
+          <section className="bio-intro">
+          <h2> About Me</h2>
             <p className="" aria-live="polite">
               {" "}
-              I am a football(soccer) fanatic, movie watcher, chef, adventurer, and a full
-              stack web developer{" "}
+                My name is Jorge! I am Full Stack Developer!
+                My stack consists of MongoDB, Express, React, and
+                Node. I am highly adaptable, and always eager to learn new
+                technologies!
+      
             </p>
-          </div>
-          <div>
-            <p className="add-bottom-padding" aria-live="polite">
-              I have experience with HTML, CSS, JAVASCRIPT, JQUERY, NODE JS, MONGOOSE, AND
-              REACT JS
+
+             <p className="add-bottom-padding" aria-live="polite">
+              I was born in Chicago, and have been in love with this city ever since.
+              I like to travel and hope one day to see the world. I love to create new things. Helping others is one of my own
+              motivations to succeed in life.
             </p>
-          </div>
-        </section>
-      </div>
+          </section>
+      
+           
+      
+        </div>
+      </section>
     );
   }
 }
--- src/main.js
+++ src/main.js
@@ -6,10 +6,19 @@ import Portfolio from "./components/portfolio";
 export default class Main extends React.Component {
   render() {
     return (
+    <div>
+
+
       <div className="main-container">
         <Bio />
+      </div>
+
+       <div className="main-container">
         <Portfolio />
       </div>
+
+
+    </div>
     );
   }
 }
