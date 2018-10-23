a7114553291072f88a39f5f6e71bc4c55e997cda
Jorge R <jorgerodriguez434@gmail.com>
2018-10-23 09:35:02 -0500

    add contact links

--- src/components/contact-info.js
+++ src/components/contact-info.js
@@ -2,14 +2,23 @@ import React from 'react';
 
 
 export default class ContactInfo extends React.Component {
+    linkedIn(){
+        window.location.assign("https://www.linkedin.com/in/jorge-rodriguez-5ba9b493/");
+    }
+    gitHub(){
+        window.location.assign("https://github.com/jorgerodriguez434");
+    }
+    mail(){
+        window.location.href = "mailto:jorgerodriguez434@gmail.com?subject=Subject&body=Jorge,";
+    }
         render = () => {
                 return <div> 
                     <h2> GET IN TOUCH </h2>
 
                     <div className="contact-pics"> 
-                        <img className="_icon"src="https://cdn4.iconfinder.com/data/icons/proglyphs-mailing-and-messaging/512/Mail_-_Reply_to_all-512.png" alt="email"/>
-                        <img className="_icon"src="http://www.pngall.com/wp-content/uploads/2016/07/Linkedin.png" alt="linked-in"/>
-                        <img className="_icon"src="https://www.shareicon.net/data/512x512/2017/03/07/880593_media_512x512.png" alt="github"/>
+                        <img onClick={this.mail} className="_icon"src="https://cdn4.iconfinder.com/data/icons/proglyphs-mailing-and-messaging/512/Mail_-_Reply_to_all-512.png" alt="email"/>
+                        <img onClick={this.linkedIn} className="_icon"src="http://www.pngall.com/wp-content/uploads/2016/07/Linkedin.png" alt="linked-in"/>
+                        <img onClick={this.gitHub} className="_icon"src="https://www.shareicon.net/data/512x512/2017/03/07/880593_media_512x512.png" alt="github"/>
                     </div>
                 </div>
         }
